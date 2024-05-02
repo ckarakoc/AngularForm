a43f8a3bdb0d6fe4283b0b6528bf554370f91bbf
Celal Karakoc <c.karakoc@live.nl>
2024-05-02 16:26:10 +0200

    Finito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { HeroFormComponent } from "./hero-form/hero-form.component";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    // CommonModule,
+    // FormsModule,
+    // BrowserModule, // Why Error?
+    HeroFormComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularForm';
+
+  ngOnInit(): void {
+    console.log('AppComponent initialized');
+  }
 }
